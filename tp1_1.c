cbc52599225bf5628243d034edb00feaa534b785
Joel Esteban Nieva Ovejero <nieva.joelesteban@gmail.com>
2025-03-30 15:31:15 -0300

    commit sobre el ejercicio de punteros en c

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int main()
 {
+    int *puntero, numero=18;
+    puntero = &numero;
     printf("Hola mundo\n");
+    printf("El contenido del puntero es: %d \n", *puntero);
+    printf("La direccion de memoria almacenada por el puntero es : %p \n", puntero);
+    printf("La direccion de memoria de la variable es %p \n", &numero);
+    printf("La direccion de memoria del puntero es: %p \n", &puntero);
+    printf("Tamanio de memoria utilizado por el puntero: %zu bytes\n", sizeof(puntero));
+    printf("Tamanio de memoria utilizado por la variable numero: %zu bytes\n", sizeof(numero));
+
     return 0;
 }
